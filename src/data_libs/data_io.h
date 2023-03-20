8ce41f8a7f432b7e557e109f69e0bc7bb511c121
Jane Charlott <janechar@ev-c3.kzn.21-school.ru>
2023-03-20 19:32:45 +0300

    2

--- src/data_libs/data_io.h
+++ src/data_libs/data_io.h
@@ -1,11 +1,10 @@
 #ifndef SRC_DATA_LIBS_DATA_IO_H_
 #define SRC_DATA_LIBS_DATA_IO_H_
 #include <stdio.h>
-#include <math.h>
+
 
 void input(double *data, int n);
 void output(double *data, int n);
-
 
 
 #endif
--- src/yet_another_decision_module/yet_another_decision_module_entry.c
+++ src/yet_another_decision_module/yet_another_decision_module_entry.c
@@ -1,9 +1,24 @@
-void main() {
-    double *data;
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../data_libs/data_io.h"
+#include "../data_module/data_process.h"
+#include "decision.h"
+
+int main() {
+    double *data = NULL;
     int n;
 
-    if (make_decision(data, n))
+    scanf("%d", &n);
+
+    data = (double *)calloc(n, sizeof(double));
+
+    input(data, n);
+
+    if (make_decision(data, n)) {
         printf("YES");
-    else
+    } else
         printf("NO");
+    free(data);
+    return 0;
 }
